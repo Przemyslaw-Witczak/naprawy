7dc135fc811abc2ece309b2e79fed0ae1691cddb
Przemek <witczakprzemyslaw@gmail.com>
2024-01-10 15:47:30 +0100

    Removed warnings

--- angularAuthorizationExample/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ angularAuthorizationExample/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -10,9 +10,10 @@ import { filter, takeUntil } from 'rxjs/operators';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent implements OnInit, OnDestroy {
-    [x: string]: any;
+  [x: string]: any;
   isExpanded = false;
   loginDisplay = false;
+  userName: string | undefined = "Not logged";
   private readonly _destroying$ = new Subject<void>();
 
   constructor(
@@ -46,6 +47,8 @@ export class NavMenuComponent implements OnInit, OnDestroy {
 
   setLoginDisplay() {
     this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+    this.userName = this.authService.instance.getActiveAccount.name;
+    console.log("Welcome, " + this.userName + "!");
   }
 
   login() {
@@ -54,6 +57,8 @@ export class NavMenuComponent implements OnInit, OnDestroy {
         this.authService.loginPopup({ ...this.msalGuardConfig.authRequest } as PopupRequest)
           .subscribe((response: AuthenticationResult) => {
             this.authService.instance.setActiveAccount(response.account);
+            this.userName = response.account.name;
+            console.log("Welcome, " + this.userName + "!");
           });
       } else {
         this.authService.loginPopup()
